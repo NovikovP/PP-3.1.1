828dabd6a7df7083b1d06e99af5819a2ea26b19a
Pavel <successdca@gmail.com>
2023-06-09 00:07:44 +0300

    Create repo

---
+++ src/main/java/ru/itm/mycrud2springboot/MyCrud2SpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.itm.mycrud2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCrud2SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCrud2SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itm/mycrud2springboot/MyCrud2SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itm.mycrud2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCrud2SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
